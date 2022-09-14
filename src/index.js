09aed9b646ce210499ac0afe0be818e5c4cf1884
Sergii Vlasenko <98455988+nepiushiu1@users.noreply.github.com>
2022-09-15 00:26:42 +0300

    первая попытка кодить

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
-// https://github.com/nepiushiu1/homework_js_10.git
+var debounce = require('lodash.debounce');
+const refs = {
+  // введеное значение
+  input: document.querySelector('#search-box'),
+};
+
+refs.input.addEventListener('input', debounce(enteredValue, DEBOUNCE_DELAY));
+
+function enteredValue(event) {
+  entered = event.target.value;
+  console.log(entered);
+}
+
+const request = fetch(`https://restcountries.com/v2/name/usa`)
+  .then(respons => {
+    return respons.json();
+  })
+  .then(information => {
+    console.log(information);
+  })
+  .catch(error => {
+    console.log(error);
+  });
